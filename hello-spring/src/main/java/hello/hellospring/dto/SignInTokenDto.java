ef284b9c437788fdcee9d06a0b1a06f4cf260f24
jin <wlsgus79622@gmail.com>
2023-09-03 21:51:25 +0900

    login return edit

--- hello-spring/src/main/java/hello/hellospring/dto/SignInTokenDto.java
+++ hello-spring/src/main/java/hello/hellospring/dto/SignInTokenDto.java
@@ -8,20 +8,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignInTokenDto {
     private String token;
-<<<<<<< HEAD
     private String name;
 
     @Builder
     public SignInTokenDto(String token, String name) {
         this.token = token;
         this.name = name;
-=======
-    private String username;
-
-    @Builder
-    public SignInTokenDto(String token,String username) {
-        this.token = token;
-        this.username = username;
->>>>>>> c88523a944e23b1df5f6040ccab6fa3c170f4645
     }
 }
